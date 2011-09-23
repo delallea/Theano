1a36c5dd138ba425e28b006fe5ed4362dd73e50b
Olivier Delalleau <delallea@iro>
2011-09-23 16:08:33 -0400

    More testing

--- theano/misc/test_numpy_mixed_cast.py
+++ theano/misc/test_numpy_mixed_cast.py
@@ -20,22 +20,30 @@ from theano import tensor
 def main():
 
     type_combinations = [
-            ('int8', 'int32'),
+            ('uint32', 'int32'),
             ]
 
     values = {
-            'int8': [0, 1, -1, 2**7 - 1, -2**7],
-            'int32': [0, 1, -1, 2**7 - 1, -2**7, 2**7, -2**7 - 1, 2**31 - 1, -2**31],
+            'uint32': [0, 1, 2],
+            'int8': [0, 1, 2, -1, 2**7 - 1, -2**7],
+            'int16': [0, 1, -1, 2**7 - 1, -2**7],
+            #'int32': [0, 1, -1, 2**7 - 1, -2**7, 2**7, -2**7 - 1, 2**31 - 1, -2**31],
+            #'int32': [1, 2**1, 2**2, 2**3, 2**4, 2**5, 2**6, 2**6 + 1, 100, 126, 2**7 - 1, 2**7],
+            #'int32': [126, 127, 128],
+            'int32': [0, 1, -1],
             }
 
-    for op in (operator.add, ):
+    for op in (
+            operator.add,
+            operator.sub,
+            ):
         for array_type, scalar_type in type_combinations:
             for array_val in values[array_type]:
                 for scalar_val in values[scalar_type]:
                     array = numpy.array([array_val] * 2, dtype=array_type)
                     scalar = numpy.array(scalar_val, dtype=scalar_type)
                     output = op(array, scalar)
-                    print '%s: %s (%s) / %s (%s) -> %s (%s)' % (
+                    print '%s: %s (%s) + %s (%s) -> %s (%s)' % (
                             op, array, array_type, scalar, scalar_type,
                             output, output.dtype)
 
